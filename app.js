d65e175a8da6b44e7e5c9fd3cef5ac5b95513fc3
osidekyle <hawkinskyle67@gmail.com>
2020-06-01 00:34:08 -0700

    js for sounds when clicked

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
 window.addEventListener('load',()=>{
     const sounds= document.querySelectorAll(".sound");
+    const pads= document.querySelectorAll(".pads div");
+
+
+
+    //Sounds
+    pads.forEach((pad,index) =>{
+        pad.addEventListener('click',function(){
+            sounds[index].play();
+        })
+    })
 });
