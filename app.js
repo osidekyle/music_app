ab32f68c73b379686d1e8928eb09d8b62c106ba1
osidekyle <hawkinskyle67@gmail.com>
2020-06-02 22:58:27 -0700

    reset currentTime, added colors list

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
 window.addEventListener('load',()=>{
     const sounds= document.querySelectorAll(".sound");
     const pads= document.querySelectorAll(".pads div");
-
+    const visual = document.querySelector(".visual");
+    const colors = ["#60e394",
+                    "#d36060" ,
+                    "#c060d3",
+                    "#d3d160",
+                    "#6860d3",
+                    "#60b2d3"]
 
 
     //Sounds
     pads.forEach((pad,index) =>{
         pad.addEventListener('click',function(){
+            sounds[index].currentTime=0;
             sounds[index].play();
-        })
-    })
+        });
+    });
 });
