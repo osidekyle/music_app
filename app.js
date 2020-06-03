b7f5d29ea0410f0e0478c99216911e2aff839ed6
osidekyle <hawkinskyle67@gmail.com>
2020-06-02 23:12:18 -0700

    added animation

--- app.js
+++ app.js
@@ -15,6 +15,20 @@ window.addEventListener('load',()=>{
         pad.addEventListener('click',function(){
             sounds[index].currentTime=0;
             sounds[index].play();
+
+            createBubble(index);
         });
     });
+
+
+    //Function that draws bubbles
+    const createBubble=(index)=>{
+        const bubble=document.createElement("div");
+        visual.appendChild(bubble);
+        bubble.style.backgroundColor=colors[index];
+        bubble.style.animation="jump 1s ease";
+        bubble.addEventListener('animationend', function(){
+            visual.removeChild(this);
+        });
+    };
 });
